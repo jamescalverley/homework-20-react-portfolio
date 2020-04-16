473d2f0fc9d960373ee89dd7bc6f5ae18b53f6e5
James Calverley <james.calverley3@gmail.com>
2020-04-16 13:34:59 -0400

    Updates

---
+++ precache-manifest.ffce9298837b817886e6f36000e1e4f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "507077cb748384d290f3dd1c057d4495",
+    "url": "/homework-20-react-portfolio/index.html"
+  },
+  {
+    "revision": "aa3b805b4599dbc33800",
+    "url": "/homework-20-react-portfolio/static/css/main.5c7591d8.chunk.css"
+  },
+  {
+    "revision": "efae23f73f2f58af669d",
+    "url": "/homework-20-react-portfolio/static/js/2.4db9b430.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/homework-20-react-portfolio/static/js/2.4db9b430.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa3b805b4599dbc33800",
+    "url": "/homework-20-react-portfolio/static/js/main.6d6146b1.chunk.js"
+  },
+  {
+    "revision": "a38bdf607fed865dc185",
+    "url": "/homework-20-react-portfolio/static/js/runtime-main.f4e21734.js"
+  }
+]);
